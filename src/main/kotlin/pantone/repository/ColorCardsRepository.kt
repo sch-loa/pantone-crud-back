b9088267295fe5d31cf223ffa1797db9d64ffbff
sch-9 <laschleichgarcia@estudiantes.unsam.edu.ar>
2023-12-12 05:53:05 -0300

    fix filter range

--- src/main/kotlin/pantone/repository/ColorCardsRepository.kt
+++ src/main/kotlin/pantone/repository/ColorCardsRepository.kt
@@ -2,6 +2,7 @@ package pantone.repository
 
 import org.springframework.stereotype.Repository
 import pantone.domain.ColorCard
+import pantone.domain.Texture
 
 @Repository
 class ColorCardsRepository {
@@ -15,6 +16,20 @@ class ColorCardsRepository {
         }
     }
 
+    fun searchByTexture(texture: Texture)= collection.filter { it.texture == texture }.toMutableSet()
+
+    fun searchByExpression(expression: String): MutableSet<ColorCard>{
+        return collection.filter {
+            it.code.toString().contains(expression) || it.hexaColor.contains(expression)
+        }.toMutableSet()
+    }
+
+    fun searchByRange(min: String?, max: String?): MutableSet<ColorCard>{
+        val filteredMin =  if(min != null) collection.filter { it.hexaColor.toInt(16) >= min.toInt(16) } else collection
+        val filteredMax =  if(max != null) collection.filter { it.hexaColor.toInt(16) <= max.toInt(16) } else collection
+
+        return filteredMin.intersect(filteredMax.toSet()).toMutableSet()
+    }
     fun update(colorCard: ColorCard){
         val card = getByID(colorCard.id)
         if( card != null){
@@ -23,6 +38,7 @@ class ColorCardsRepository {
             card.hexaColor = colorCard.hexaColor
         }
     }
+
     fun delete(colorCard: ColorCard){ collection.remove(colorCard) }
     private fun assignId() = if(collection.isEmpty()) 1 else collection.maxOf { it.id } + 1
     private fun isNew(colorCard: ColorCard) = colorCard.id == 0
--- src/main/kotlin/pantone/services/ColorCardsService.kt
+++ src/main/kotlin/pantone/services/ColorCardsService.kt
@@ -3,6 +3,7 @@ package pantone.services
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import pantone.domain.ColorCard
+import pantone.domain.Texture
 import pantone.repository.ColorCardsRepository
 
 @Service
@@ -10,7 +11,17 @@ class ColorCardsService{
     @Autowired
     private lateinit var colorCardsRepository: ColorCardsRepository
 
-    fun colorCards(filters: Map<String, String>) = colorCardsRepository.collection
+    private var filterCalls: Map<String, (String) -> MutableSet<ColorCard>> = mapOf(
+        "texture" to { value -> colorCardsRepository.searchByTexture(Texture.valueOf(value)) },
+        "search" to { value -> colorCardsRepository.searchByExpression(value) },
+        "min_color" to { value -> colorCardsRepository.searchByRange(value, null)},
+        "max_color" to { value -> colorCardsRepository.searchByRange(null, value)}
+    )
+
+    fun colorCards(filters: Map<String, String>): MutableSet<ColorCard> {
+        val fetchedCards = filters.map { (key, value) -> filterCalls[key]?.invoke(value) ?: mutableSetOf() }
+        return fetchedCards.reduce { accCards, cards -> accCards.intersect(cards).toMutableSet() }.toMutableSet()
+    }
 
     fun createColorCard(card: ColorCard){
         colorCardsRepository.create(card)
